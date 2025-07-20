54d33af88ac646cb78c9a5ba66ea936b051fe456
Alya Triswani <153605534+AlyaTriswani14@users.noreply.github.com>
2025-07-20 16:51:28 +0700

    Add files via upload

---
+++ 2223-ga-t06-dorm-buddies-AlyaTriswani/2223-ga-t05-freebies-AlyaTriswani/X02.java
@@ -0,0 +1,41 @@
+// 12S23048 - Grace Caldera Situmorang
+// 12S23038- Alya Triswani
+
+import java.util.*;
+import java.lang.Math;
+
+public class X02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] nama = new String[4];
+        int i;
+
+        i = 0;
+        func_default(i, nama);
+        inputbuddies(i, nama);
+        outputbuddies(i, nama);
+    }
+    
+    public static void func_default(int i, String[] nama) {
+        for (i = 0; i <= 3; i++) {
+            nama[i] = "";
+        }
+    }
+    
+    public static void inputbuddies(int i, String[] nama) {
+        for (i = 0; i <= 3; i++) {
+            nama[i] = input.nextLine();
+            if (nama[i].equals("---")) {
+                nama[i] = "";
+                i = 3;
+            }
+        }
+    }
+    
+    public static void outputbuddies(int i, String[] nama) {
+        for (i = 0; i <= 3; i++) {
+            System.out.println(nama[i]);
+        }
+    }
+}
